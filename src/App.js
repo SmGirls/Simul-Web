93024b8d47183e1dac9cefe0ef4577997892f6a7
Jeongjewww <gwmelody99@gmail.com>
2023-07-14 16:33:53 +0900

    add table front code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './css/reset.css'
-import './App.css'
 import Main from './pages/CsvDetail/CsvDetail'
 import CsvDetail from './pages/CsvDetail/CsvDetail'
+import Table from './components/Table/ItemTable'
+import { useMemo } from 'react';
 
 function App() {
+  const tableData = [];
+
+  const columns = useMemo(
+    () => [
+      {
+        accessor: "Product ID",
+        Header: "id",
+      },
+      {
+        accessor: "Product Name",
+        Header: "namefke",
+      },
+      {
+        accessor: "가로",
+        Header: "rows",
+      },
+      {
+        accessor: "세로",
+        Header: "columns",
+      },
+      {
+        accessor: "높이",
+        Header: "height",
+      },
+      {
+        accessor: "무게",
+        Header: "weight",
+      },
+    ],
+    []
+  );
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/csv" element={<CsvDetail />}></Route>
+        <Route path="/table" element={<Table columns={columns} data={tableData} />}></Route>
       </Routes>
     </BrowserRouter>
+
   )
 }
 
